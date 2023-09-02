44c1a308d5a257d5e26380a42ba9140b79ab257f
Sadrah_Alsabbagh <sedrasabbagh33@gmail.com>
2023-09-02 12:57:48 +0300

    add foyr items to cart instead of one

--- cypress/e2e/03-cypress_checkout.js
+++ cypress/e2e/03-cypress_checkout.js
@@ -16,13 +16,28 @@
             cy.get('#loginFrm_password').type(password);
             cy.get('#loginFrm > fieldset > .btn > .fa').click();
             cy.get('.logo > img').click();
-            
 //add items
+//item 1
 cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(1) > .thumbnail > :nth-child(1) > img').click()
     cy.get('.cart').click();
+    cy.get('.breadcrumb > :nth-child(1) > a').click();
+            //item2
+        cy.get('#block_frame_featured_1769 > .thumbnails > :nth-child(3) > .thumbnail > :nth-child(1) > img').click();
+        cy.get('.cart').click();
+        cy.get('.breadcrumb > :nth-child(1) > a').click();
+                //item3
+        cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(1) > .thumbnail > :nth-child(1) > img').click();
+        cy.get('.cart').click();
+        cy.get('.breadcrumb > :nth-child(1) > a').click();    
+        //item4
+        cy.get('#block_frame_latest_1770 > .thumbnails > :nth-child(2) > .thumbnail > :nth-child(2) > img').click();
+        cy.get('.cart').click();
+
+        //checkout
     cy.get('#cart_checkout1').click();
     cy.get('#checkout_btn').click();
-            cy.get('.maintext').should('include','processed!')
+    cy.get('.maintext').should('contain','Your Order Has Been Processed!')
+
 
         });
      
